cecbec3a7131557d1ef446a0ecbe70c1660ca1a5
Mati <matipacheco@gmail.com>
2019-06-18 23:01:55 -0400

    ill forget about this for now

--- app/lib/pub_sub.rb
+++ app/lib/pub_sub.rb
@@ -4,9 +4,9 @@ module PubSub
 
   @chat_room = ChatRoom.instance.chat_room
 
-  def self.publish channel = ENV["CHAT_ROOM_CHANNEL"], body
+  def self.publish body
     begin
-      @chat_room.publish channel, body.to_json
+      @chat_room.publish ENV["CHAT_ROOM_CHANNEL"], body.to_json
 
     rescue Exception => e
       e
